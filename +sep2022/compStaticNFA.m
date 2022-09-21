8db8d0c90b33497b764cacc2ae075b697f43c2fe
Jaromir Benes <jaromir.benes@gmail.com>
2022-09-21 10:57:02 +0200

    Added NFA simulations

--- +sep2022/compStaticNFA.m
+++ +sep2022/compStaticNFA.m
@@ -6,13 +6,27 @@ clear
 load ./+model/+symmetric2A/model.mat m2
 ms = m2;
 
+ms.aa_nu_1 = 0;
+ms.bb_nu_1 = 0;
+ms = steady(ms, "fixLevel", ["gg_a", "gg_nt", "aa_pc", "bb_pc"]);
+
+
 ms1 = ms;
 ms1.aa_beta = 0.97;
-% ms1 = steady(ms1, "fixLevel", ["gg_a", "gg_nt", "aa_pc", "bb_pc"]);
+ms1 = steady(ms1, "fixLevel", ["gg_a", "gg_nt", "aa_pc", "bb_pc"]);
+checkSteady(ms1);
+
+s1 = access(ms1, "steady-level");
+
+[
+s1.aa_nfa_to_ngdp * s1.aa_ngdp
+s1.bb_nfa_to_ngdp * s1.bb_ngdp / s1.bb_e
+]
+
+
 
 ms2 = ms;
 ms2.aa_ss_nw_to_nn = ms2.aa_ss_nw_to_nn - 0.05;
-% ms2.aa_nu_1 = 0.10;
 ms2 = steady(ms2, "fixLevel", ["gg_a", "gg_nt", "aa_pc", "bb_pc"]);
 checkSteady(ms2);
 

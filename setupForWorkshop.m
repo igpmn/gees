9e2ca32e419b32ff7e05cc92266eb964a8933722
Jaromír Beneš <jaromir.benes@gmail.com>
2023-01-30 11:46:40 +0200

    Added setup script

---
+++ setupForWorkshop.m
@@ -0,0 +1,30 @@
+
+cd(userpath)
+disp("    Setting up workshop repositories in " + pwd())
+
+geesDir = "igpmn-gees";
+irisDir = "iris-toolbox";
+timeStamp = string(datestr(now(), 30));
+
+saveDirectoryWhenNeeded(geesDir, timeStamp);
+saveDirectoryWhenNeeded(irisDir, timeStamp);
+
+status = system("git clone --filter=blob:none https://github.com/igpmn/gees.git " + geesDir);
+if status~=0
+    error("Cannot clone IGPMN GEES model repository.");
+end
+
+status = system("git clone --filter=blob:none https://github.com/IRIS-Solutions-Team/IRIS-Toolbox.git " + irisDir);
+if status~=0
+    error("Cannot clone Iris Toolbox repository.");
+end
+
+
+function saveDirectoryWhenNeeded(dir, stamp)
+    if exist(dir, "dir")
+        dirStamped = dir + "-" + stamp;
+        disp("    Moving the existing "+dir+" to "+dirStamped);
+        movefile(dir, dirStamped);
+    end
+end%
+

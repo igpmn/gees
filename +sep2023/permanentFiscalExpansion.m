6fbfaf5e748429d65f8b17f51397d8972a7db1e1
Jaromír Beneš <jaromir.benes@gmail.com>
2023-09-08 10:55:46 +0200

    Fixed the order of execution

--- +sep2023/permanentFiscalExpansion.m
+++ +sep2023/permanentFiscalExpansion.m
@@ -15,16 +15,36 @@ simulationRange = startSimulation : endSimulation;
 
 m = model.global4A.create();
 checkSteady(m);
-d = databank.forModel(m, simulationRange);
 
+
+%%
 
 %
 % Create an economy with a new level of govt debt, and recalibrate fiscal
 % reaction parameters
 %
 
-m1 = m;
+startSimulation = 1;
+endSimulation = 100;
+simulationRange = startSimulation : endSimulation;
 areas = accessUserData(m, "areas");
+
+m = alter(m, 2);
+
+% Increase curr income effect in parameter variant #2, switch off in #1
+for a = areas
+    m.(a+"_chi_curr") = [0, 0.7];
+    m.(a+"_nu_1") = [0, 0.07];
+end
+
+fix = accessUserData(m, "fix");
+m = steady(m, "fixLevel", fix);
+checkSteady(m);
+m = solve(m);
+
+d = databank.forModel(m, simulationRange);
+
+% Assign new parameter in both parameter variants
 for a = areas
     m1.(a+"_ss_dg_to_ngdp") = m1.(a+"_ss_dg_to_ngdp") + 0.20;
     m1.(a+"_tau_cg") = 3;
@@ -62,6 +82,7 @@ s1 = simulate( ...
     m1, d1, simulationRange ...
     , "prependInput", true ...
     , "method", "stacked" ...
+    , "solver", "quickNewton" ...
     , "plan", p ...
 );
 
@@ -76,5 +97,5 @@ reportTitle = "Permanent fiscal expansion";
 legend = ["Global 20%"];
 fileName = "+sep2023/html/permanent-fiscal-expansion.html";
 
-report.basic(m, smc1, simulationRange, reportTitle, legend, fileName);
+report.basic(m, smc1, 1:20, reportTitle, legend, fileName);
 

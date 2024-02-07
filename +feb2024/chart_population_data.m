72db90ce83245389e080100e302e427739b8b1de
tomas.motl <tomas.motl@ogresearch.com>
2024-02-07 23:37:47 +0800

    added more countries to demographic charts

--- +feb2024/chart_population_data.m
+++ +feb2024/chart_population_data.m
@@ -11,6 +11,7 @@ db = databank.merge("error", wpp, lfs);
 sl4 = access(m4, "steady-level");
 
 areas = ["gg", "us", "ea", "cn", "rw"];
+other_countries = ["az", "id", "eg", "za", "ma", "ph"];
 
 c = [
          0    0.4470    0.7410
@@ -25,6 +26,20 @@ for aa = areas
     c = circshift(c, 1);
 end
 
+c2 = [
+         0    0.4470    0.7410
+    0.8500    0.3250    0.0980
+    0.9290    0.6940    0.1250
+    0.4940    0.1840    0.5560
+    0.4660    0.6740    0.1880
+    0.30      0.4540    0.10
+];
+colors2 = struct();
+for aa = other_countries
+    colors2.(aa) = c2(1, :);
+    c2 = circshift(c2, 1);
+end
+
 plotRange = db.startHist : db.endProj;
 projRange = db.startProj : db.endProj;
 
@@ -34,11 +49,21 @@ figure();
 hold on;
 hs = [];
 for aa = areas
-    hs(end+1) = plot(plotRange, pct([db.(aa + "_nn")]));
+    hs(end+1) = plot(plotRange, pct([db.(aa + "_nn")]),'LineWidth',3);
 end
-plot(plotRange, Series(plotRange, 100*(sl4.gg_ss_roc_nt-1)), "--", color="black");
-title("Rate of change in total population");
+plot(plotRange, Series(plotRange, 100*(sl4.gg_ss_roc_nt-1)), "--", "color", "black",'LineWidth',3);
+title("Rate of change in total population - model areas");
 legend(hs, upper(areas));
+visual.highlight(projRange);
+
+figure();
+hold on;
+hs = [];
+for aa = other_countries
+    hs(end+1) = plot(plotRange, pct([db.(aa + "_nn")]),'LineWidth',3);
+end
+title("Rate of change in total population - other countries");
+legend(hs, upper(other_countries));
 visual.highlight(projRange);
 
 figure();
@@ -51,16 +76,31 @@ for aa = areas
         data = Series();
     end
     try
-        model = 100*Series(plotRange, sl4.(aa + "_ss_nw_to_nn"));
+        modelss = 100*Series(plotRange, sl4.(aa + "_ss_nw_to_nn"));
     catch
-        model = Series();
+        modelss = Series();
     end
-    hs(end+1) = plot(plotRange, data, color=colors.(aa), marker="s");
-    plot(plotRange, model, "--", color=colors.(aa));
+    hs(end+1) = plot(plotRange, data, "color", colors.(aa), "marker", "s", 'LineWidth',3);
+    plot(plotRange, modelss, "--", "color", colors.(aa), 'LineWidth',3);
 end
 visual.highlight(projRange);
 legend(hs, upper(areas));
-title("Working age to total population");
+title("Working age to total population - model areas");
+
+figure();
+hold on;
+hs = [];
+for aa = other_countries
+    try
+        data = 100*db.(aa + "_nw_to_nn");
+    catch
+        data = Series();
+    end
+    hs(end+1) = plot(plotRange, data, "color", colors2.(aa), "marker", "s", 'LineWidth',3);
+end
+visual.highlight(projRange);
+legend(hs, upper(other_countries));
+title("Working age to total population - other countries");
 
 figure();
 hold on;
@@ -72,23 +112,38 @@ for aa = areas
         data = Series();
     end
     try
-        model = 100*Series(plotRange, sl4.(aa + "_ss_nf_to_nw"));
+        modelss = 100*Series(plotRange, sl4.(aa + "_ss_nf_to_nw"));
     catch
-        model = Series();
+        modelss = Series();
     end
-    hs(end+1) = plot(plotRange, data, color=colors.(aa));
-    plot(plotRange, model, "--", color=colors.(aa));
+    hs(end+1) = plot(plotRange, data, "color", colors.(aa) ,'LineWidth',3);
+    plot(plotRange, modelss, "--", "color", colors.(aa), 'LineWidth',3);
 end
 legend(hs, upper(areas));
 visual.highlight(projRange);
-title("Labor force participation rate");
+title("Labor force participation rate - model areas");
+
+figure();
+hold on;
+hs = [];
+for aa = other_countries
+    try
+        data = 100*db.(aa + "_nf_to_nw");
+    catch
+        data = Series();
+    end
+    hs(end+1) = plot(plotRange, data, "color", colors2.(aa) ,'LineWidth',3);
+end
+legend(hs, upper(other_countries));
+visual.highlight(projRange);
+title("Labor force participation rate - other countries");
 
 
 figure();
 tiledlayout( ...
     "flow" ...
-    , tileSpacing = "compact" ...
-    , padding = "compact" ...
+    , "tileSpacing", "compact" ...
+    , "padding", "compact" ...
 );
 for aa = areas
     nexttile();
@@ -100,4 +155,19 @@ for aa = areas
     legend("0-14", "Working age", "65+");
 end
 
+figure();
+tiledlayout( ...
+    "flow" ...
+    , "tileSpacing", "compact" ...
+    , "padding", "compact" ...
+);
+for aa = other_countries
+    nexttile();
+    expression = replace("100*[?_n14, ?_nw, ?_n65] / ?_nn", "?", aa);
+    data = databank.eval(db, expression);
+    area(plotRange, data);
+    visual.highlight(projRange);
+    title(upper(aa) + ": Demographic distribution");
+    legend("0-14", "Working age", "65+");
+end
 
--- +feb2024/create_model.m
+++ +feb2024/create_model.m
@@ -2,7 +2,7 @@
 
 clear
 
-load("+model/+global4a/model.mat", "m4");
+load("+model/+global4A/model.mat", "m4");
 
 save +feb2024/mat/global4a.mat m4
 
